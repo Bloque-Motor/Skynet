a2ec5a3bcc8870eeeb71a0a13ae42cdb15375758
inigoar <inigoarpalacios@yahoo.es>
2020-04-26 17:34:54 +0200

    loop y updated

--- PruebaArchivos.py
+++ PruebaArchivos.py
@@ -4,25 +4,34 @@ import ntpath
 
 path = 'Data/'
 
-subfolders = [ f.name for f in os.scandir(path) if f.is_dir() ]
+subfolders = [ f.path for f in os.scandir(path) if f.is_dir() ]
 
 for x in range(len(subfolders)):
 
-   locutor = subfolders[x]
-   ruta = locutor + "/*"
+    subfoldersSpeakers = [f.name for f in os.scandir(subfolders[x]) if f.is_dir()]
+    sentence = ntpath.basename(subfolders[x])
+    for y in range (len(subfoldersSpeakers)):
 
-   for filename in glob.glob(os.path.join(path, ruta)):
-    with open(os.path.join(os.getcwd(), filename), 'r') as f:
+        locutor = subfoldersSpeakers[y]
+        print(locutor)
+        ruta = locutor + "/*"
+        path = subfolders[x]
+        for filename in glob.glob(os.path.join(subfolders[x], ruta)):
+            with open(os.path.join(os.getcwd(), filename), 'r') as f:
 
-      print(ntpath.basename(filename))
+                #print(ntpath.basename(filename))
 
-      outputName = "output_" + ntpath.basename(filename)
+                outputName = "output_" + ntpath.basename(filename)
 
-      filepath = os.path.join('Output/', outputName)
-      if not os.path.exists('Output/'):
-          os.makedirs('Output/')
-      f = open(filepath, "a")
-      f.close()
+                filepath = os.path.join('Output/', outputName)
+                if not os.path.exists('Output/'):
+                    os.makedirs('Output/')
+                f = open(filepath, "w")
+                f.close()
+
+
+
+
 
 
 

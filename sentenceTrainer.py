0e759af52d4bcf1f6c384ddd06ef6d6d8de7e255
inigoar <inigoarpalacios@yahoo.es>
2020-04-26 19:16:26 +0200

    sentenceTrainer class

--- sentenceTrainer.py
+++ sentenceTrainer.py
@@ -3,3 +3,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
+classesSentence = ['S1','S2','S3','S4','S5']
+num_Sentences = lent(classesSentence)
+
+train_path = 'Data'

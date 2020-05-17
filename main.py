759632a2cb7af2486d27aecda303ecda00dc2928
inigoar <inigoarpalacios@yahoo.es>
2020-05-17 20:36:28 +0200

    all working

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import warnings
 import predictor
-import extractData
+import SpeakerData
 
 
 def main():
     print('Bienvenido a Aplicaciones de la biometría de la voz.')
     warnings.filterwarnings("ignore", category=RuntimeWarning)
     #predictor.analyze('Data/S4/spk_000935/trn_059924.wav')
-    extractData
+    SpeakerData
 
 if __name__ == "__main__":
     main()

bcb6471350d148a6aeee03850cb2549b44e8136f
inigoar <inigoarpalacios@yahoo.es>
2020-05-17 20:27:32 +0200

    fixed

--- SpeakerData.py
+++ SpeakerData.py
@@ -27,6 +27,7 @@ for filename in os.listdir(sourcePath):
 
 speakers = [f.path for f in os.scandir(sourcePath) if f.is_dir()] # cuidado con ejecutar dos veces que toma train y validate como speakers, se podria hacer un remove despues
 arr = []
+
 for x in range(len(speakers)):
     speakerNumber = ntpath.basename(speakers[x])
     directory = os.listdir(sourcePath + speakerNumber +"/")
@@ -42,11 +43,22 @@ print(arr[0:4]) #poner el numero de directorios que mostrar
 for y in arr[:4]: #poner el numero 25
 
     speakersPath = y[0]
-    files = [f.path for f in os.scandir(sourcePath + speakersPath + "/") if f.is_file()]
+
+    for filename in os.listdir(sourcePath + speakersPath + "/"):
+
+        if filename.endswith(".png"):
+
+            speaker = filename[4:10]
+            dest = destinationTrain + "spk_" + speaker + "/"
+            if not os.path.exists(dest):
+                os.makedirs(dest)
+
+            shutil.move(sourcePath + speakersPath + "/" + filename, dest, )
+
+
+    files = [f.path for f in os.scandir(destinationTrain + speakersPath + "/") if f.is_file()]
     num_files = len(files)
-    #print(num_files)
     val_num = max(math.ceil(0.2 * num_files), 1)
-    #print(val_num)
     to_move = []
 
     for x in range(0, val_num):
@@ -59,11 +71,14 @@ for y in arr[:4]: #poner el numero 25
                 repeated = False
 
     for file in to_move:
-        speaker = file[23:29]
+        speaker = file[29:35]
         dest = destinationValidate + "spk_" + speaker + "/"
         if not os.path.exists(dest):
             os.makedirs(dest)
-        shutil.move(file, dest,)
+        shutil.move(file, dest, )
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import warnings
 import predictor
-import SpeakerData
 import extractData
 
 
@@ -8,7 +7,7 @@ def main():
     print('Bienvenido a Aplicaciones de la biometría de la voz.')
     warnings.filterwarnings("ignore", category=RuntimeWarning)
     #predictor.analyze('Data/S4/spk_000935/trn_059924.wav')
-    SpeakerData
+    extractData
 
 if __name__ == "__main__":
     main()
